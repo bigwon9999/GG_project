7c8d515a652f21918174059f7e351a2e270ad4a3
jsh5408 <jsh5408@naver.com>
2021-10-13 18:39:43 +0900

    test2 -> test

--- js/test.js
+++ js/test.js
@@ -1,41 +1,44 @@
-let $button = document.querySelector("#stBtn");
-//html에서 id가 stBtn인 태그를 $button 변수에 연결
-let $qBox = document.querySelector(".qBox");
-//class가 .qBox인 태그를 $qBox 변수에 연결
-let $ansA = document.querySelector("#ansA");
-let $ansB = document.querySelector("#ansB");
-let $ansC = document.querySelector("#ansC");
-let $title = document.querySelector("#title");
-let $prev = document.querySelector("#prev");
-let $next = document.querySelector("#next");
-let qIdx = 0;
-function begin(){
-const onClick = () => {
-  console.log("테스트 시작");
-  $title.textContent = ' 테스트가 시작되었습니다.';
-}
-$button.addEventListener("click", onClick);
-//$button 에 eventListener을 추가해서 click 이벤트 발생 시 onClick함수 실행
-//이 때 함수는 es6문법인 화살표함수(익명함수)로 만듦
+const qnaSection = document.getElementById("qna");
+const title = document.getElementById("title");
+const stBtn = document.getElementById("stBtn");
+const start = document.getElementById("start");
+const qBox = document.querySelector(".qBox");
+const aBox = document.querySelector(".aBox");
+const answer = document.querySelectorAll(".answer");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
 
-goNext(qIdx);
+let qIdx = 0;
+
+function beginTest() {
+    title.innerText ="테스트 시작되었어요";
+    stBtn.style.display = "none";
+    showQuestion(qIdx);
 }
-function goPrev(qIdx){
-  for(var el of qnaListss){
-    $qBox.innerHTML = qnaListss[el];
-    if(el === 0){
-      alert("안됩니다");
-      el = 0;
+
+function showQuestion(qIdx) {
+    //console.log(qnaListss[qIdx])
+    qBox.innerText = qnaListss[qIdx].q;
+    const ans = qnaListss[qIdx].a;
+    for(let i = 0; i < ans.length; i++){
+        answer[i].innerText = ans[i].answer
     }
-  }
-  $prev.addEventListener("click",goPrev);
 }
-function goNext(qIdx){
-  console.log(qIdx);
-  console.log(qnaListss[qIdx].q);
-  $qBox.innerHTML = qnaListss[qIdx].q;
-  for(let i in qnaListss[qIdx].a){
-    addAnswer(qnaListss[qIdx].a[i].answer, qIdx);
-  }
-  $next.addEventListener("click",goNext(qidx++));
+
+function onPrev() {
+    if (qIdx > 0) {
+        qIdx -= 1
+        showQuestion(qIdx);
+    }
 }
+
+function onNext() {
+    if (qIdx < 9) {
+        qIdx += 1
+        showQuestion(qIdx);
+    }
+}
+
+prevBtn.addEventListener("click", onPrev);
+nextBtn.addEventListener("click", onNext);
+stBtn.addEventListener("click", beginTest);
